0e18ab205de5d795b3278845634d3e04973308fa
GeraGonzz <fibonappi@gmail.com>
2020-10-12 13:06:55 -0500

    Updates

---
+++ precache-manifest.a29ce94412a7694c0efa9cfa60daa766.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c559d5ee256678d81f299835bc1ccb56",
+    "url": "/react-git-app/index.html"
+  },
+  {
+    "revision": "63244515ced5f6bf171b",
+    "url": "/react-git-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "63f7c60f5c14e6a91079",
+    "url": "/react-git-app/static/js/2.a62bece4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-git-app/static/js/2.a62bece4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63244515ced5f6bf171b",
+    "url": "/react-git-app/static/js/main.f75aba7b.chunk.js"
+  },
+  {
+    "revision": "892befb734b6f588c382",
+    "url": "/react-git-app/static/js/runtime-main.09da495f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-git-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
